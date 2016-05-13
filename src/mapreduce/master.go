efb7a150b8a982e51b2fcbc033c1a840a311d2d1
frankobe <mua08p@gmail.com>
2016-05-13 11:39:02 -0700

    Lab 1:Part 2&3 passed

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -3,12 +3,10 @@ package mapreduce
 import "container/list"
 import "fmt"
 
-
 type WorkerInfo struct {
 	address string
 	// You can add definitions here.
 }
-
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
@@ -30,5 +28,62 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+	idleWKChannel := make(chan string)
+	jobChannel := make(chan *DoJobArgs)
+	doneChannel := make(chan int)
+
+	defer close(jobChannel)
+	defer close(doneChannel)
+
+	getNextWorkerAddr := func() string {
+		var addr string
+		select {
+		case addr = <-mr.registerChannel:
+			mr.Workers[addr] = &WorkerInfo{addr}
+		case addr = <-idleWKChannel:
+		}
+		return addr
+	}
+
+	sendJob := func(workeraddr string, job *DoJobArgs) {
+		var reply DoJobReply
+		ok := call(workeraddr, "Worker.DoJob", job, &reply)
+		if ok {
+			doneChannel <- 1
+			idleWKChannel <- workeraddr
+		} else {
+			jobChannel <- job
+		}
+	}
+	// distribute jobs to available workers
+	go func() {
+		for job := range jobChannel {
+			workeraddr := getNextWorkerAddr()
+			go sendJob(workeraddr, job)
+		}
+	}()
+
+	go func() {
+		for i := 0; i < mr.nMap; i++ {
+			job := &DoJobArgs{mr.file, Map, i, mr.nReduce}
+			jobChannel <- job
+		}
+	}()
+
+	for i := 0; i < mr.nMap; i++ {
+		<-doneChannel
+	}
+
+	go func() {
+		for i := 0; i < mr.nReduce; i++ {
+			job := &DoJobArgs{mr.file, Reduce, i, mr.nMap}
+			jobChannel <- job
+		}
+	}()
+
+	for i := 0; i < mr.nReduce; i++ {
+		<-doneChannel
+	}
+
 	return mr.KillWorkers()
 }
